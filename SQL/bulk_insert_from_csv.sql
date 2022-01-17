615c86082b83e33aa8b393c6cc6210c1587d8614
kit-tie <32528469+kit-tie@users.noreply.github.com>
2022-01-17 17:04:34 +0200

    Code change bulk insert
    FilePath and FileName

--- SQL/bulk_insert_from_csv.sql
+++ SQL/bulk_insert_from_csv.sql
@@ -17,9 +17,7 @@ CREATE TABLE [dbo].[raw.Orders](
 )
 GO
 
---''C:\Users\Kateryna_Muraviova\Downloads\Import_error_file.csv'',
-
-CREATE PROCEDURE import_csv_test (@FilePath VARCHAR(MAX), @FileName VARCHAR(MAX))
+CREATE OR ALTER PROCEDURE import_csv_test (@FilePath VARCHAR(MAX), @FileName VARCHAR(MAX))
 AS
 BEGIN
 
@@ -44,20 +42,6 @@ GO
 USE [Practice]
 GO
 DECLARE @FilePath VARCHAR(MAX), @FileName VARCHAR(MAX)
-SET  @FilePath = 'D:\S3\bulk_insert\'
-SET  @FileName = 'data.csv'
+SET  @FilePath = 'C:\Katie\DEMO_DWH\Sample_files'
+SET  @FileName = 'SampleCSVFile_53000kb.csv'
 EXECUTE [dbo].[import_csv_test]  @FilePath, @FileName
-
-SELECT CONNECTIONPROPERTY('local_net_address') AS [IP]
-
-exec msdb.dbo.rds_gather_file_details 
-SELECT * FROM msdb.dbo.rds_fn_list_file_details(28)
-EXEC msdb..rds_task_status @task_id = 25
-
-exec msdb.dbo.rds_download_from_s3
-      @s3_arn_of_file='arn:aws:s3:::testbuck987/myfile.csv',
-      @rds_file_path='D:\S3\bulk_insert\data.csv',
-      @overwrite_file=1;
-
-exec msdb.dbo.rds_delete_from_filesystem
-    @rds_file_path='D:\S3\bulk_insert\data.csv_error.csv';
